06cf04ed9985a3290f382d81131999cdad964d7a
Shaw Terwilliger <sterwill@tinfig.com>
2017-02-19 13:27:07 -0500

    Upload script resets and uploads

---
+++ tvipt/upload.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e 
+#
+# Manpage for "arduino" command:
+#
+# https://github.com/arduino/Arduino/blob/master/build/shared/manpage.adoc
+
+ARDUINO_HOME="$(dirname $(which arduino))"
+BASE="$(realpath $(dirname ${0}))"
+BUILD_PATH="${BASE}/build"
+
+DEV="/dev/ttyACM0"
+
+if ! [ -z "${1}" ] ; then
+  DEV="${1}"
+  shift
+fi
+
+stty -F "${DEV}" speed 1200 hupcl 1>/dev/null
+sleep 1.5
+"${HOME}/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac" \
+  -i --port="$(basename ${DEV})" -U true -i -e -w \
+  "${BUILD_PATH}/tvipt.ino.bin" -R 

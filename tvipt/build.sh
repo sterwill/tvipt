f1ce3873b63ebfa7ddc1bb80d042d2b3c5abb828
Shaw Terwilliger <sterwill@tinfig.com>
2017-02-19 12:40:39 -0500

    Some simple scripts to build

---
+++ tvipt/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e 
+#
+# Manpage for "arduino" command:
+#
+# https://github.com/arduino/Arduino/blob/master/build/shared/manpage.adoc
+
+ARDUINO_HOME="$(dirname $(which arduino))"
+BASE="$(realpath $(dirname ${0}))"
+BUILD_PATH="${BASE}/build"
+
+mkdir -p "${BUILD_PATH}"
+
+arduino-builder -compile \
+  -hardware "${ARDUINO_HOME}/hardware" \
+  -hardware "${HOME}/.arduino15/packages" \
+  -tools "${ARDUINO_HOME}/tools-builder" \
+  -tools "${ARDUINO_HOME}/hardware/tools/avr" \
+  -tools "${HOME}/.arduino15/packages" \
+  -built-in-libraries "${ARDUINO_HOME}/libraries" \
+  -libraries "${HOME}/Arduino/libraries" \
+  -fqbn=adafruit:samd:adafruit_feather_m0 \
+  -vid-pid=0X239A_0X800B \
+  -ide-version=10801 \
+  -build-path "${BUILD_PATH}" \
+  -warnings=none \
+  -prefs=build.warn_data_percentage=75  \
+  -prefs="runtime.tools.openocd.path=${HOME}/.arduino15/packages/arduino/tools/openocd/0.9.0-arduino"  \
+  -prefs="runtime.tools.bossac.path=${HOME}/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino" \
+  -prefs="runtime.tools.CMSIS.path=${HOME}/.arduino15/packages/arduino/tools/CMSIS/4.0.0-atmel" \
+  -prefs="runtime.tools.arm-none-eabi-gcc.path=${HOME}/.arduino15/packages/arduino/tools/arm-none-eabi-gcc/4.8.3-2014q1" \
+  "${BASE}/tvipt.ino"
--- tvipt/deps.sh
+++ tvipt/deps.sh
@@ -1,26 +1,9 @@
 #!/bin/sh -e 
+#
+# Manpage for "arduino" command:
+#
+# https://github.com/arduino/Arduino/blob/master/build/shared/manpage.adoc
 
-BASE=$(dirname $0)
-DEPS_DIR="${BASE}/deps"
-DL_DIR="${BASE}/deps/dl"
-
-get_dep() {
-  URL="$1"
-  EXTRACT_AT="$2"
-
-  # Hacky, but works OK for our files
-  FILE="${DL_DIR}/$(basename ${URL})"
-
-  if ! [ -f "${FILE}" ] ; then
-    mkdir -p "${DL_DIR}"
-    echo "${URL} -> ${FILE}"
-    curl -L -s -o "${FILE}" "${URL}"
-    tar -x -v -C "${EXTRACT_AT}" -f "${FILE}"
-  fi
-}
-
-
-get_dep "https://github.com/arduino/arduino-builder/releases/download/1.3.23/arduino-builder-linux64-1.3.23.tar.bz2" "${DEPS_DIR}"
-
-# https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
-get_dep "https://adafruit.github.io/arduino-board-index/boards/adafruit-samd-1.0.13.tar.bz2" "${DEPS_DIR}/hardware"
+arduino --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json" --save-prefs
+arduino --install-library WiFi101:0.12.1
+arduino --install-boards adafruit:samd:1.0.13

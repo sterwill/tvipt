4f4a69e9be157b31497ce14a837b9d5493c3ef34
Shaw Terwilliger <sterwill@tinfig.com>
2017-06-04 17:38:52 -0400

    Upgrade deps

--- tvipt/deps.sh
+++ tvipt/deps.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -e 
+#!/bin/sh 
 #
 # Manpage for "arduino" command:
 #
 # https://github.com/arduino/Arduino/blob/master/build/shared/manpage.adoc
 
 arduino --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json" --save-prefs
-arduino --install-library WiFi101:0.14.0
-arduino --install-boards adafruit:samd:1.0.13
+arduino --install-library WiFi101:0.14.3
+arduino --install-boards adafruit:samd:1.0.17

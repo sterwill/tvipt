5626440aeaf2036ef03a790ed6dc1c596888886c
Shaw Terwilliger <sterwill@tinfig.com>
2018-01-07 10:46:05 -0500

    Upgrade WiFi to 0.15.0

--- tvipt/deps.sh
+++ tvipt/deps.sh
@@ -5,5 +5,5 @@
 # https://github.com/arduino/Arduino/blob/master/build/shared/manpage.adoc
 
 arduino --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json" --save-prefs
-arduino --install-library WiFi101:0.14.3
+arduino --install-library WiFi101:0.15.0
 arduino --install-boards adafruit:samd:1.0.17

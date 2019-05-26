98bdc407a9137e49d71c82f425138a35428ba2a9
Shaw Terwilliger <sterwill@tinfig.com>
2019-05-26 13:31:47 -0400

    Latest libraries

--- tvipt/deps.sh
+++ tvipt/deps.sh
@@ -7,7 +7,7 @@
 git clone git@github.com:sterwill/CifraChaCha20Poly1305.git ~/Arduino/libraries/CifraChaCha20Poly1305
 
 arduino --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json" --save-prefs
-arduino --install-library WiFi101:0.15.2
-arduino --install-boards arduino:samd:1.6.18
-arduino --install-boards adafruit:samd:1.0.22
+arduino --install-library WiFi101:0.15.3
+arduino --install-boards arduino:samd:1.6.21
+arduino --install-boards adafruit:samd:1.4.1
 
